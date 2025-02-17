7ad81915e5caa4a8c9d1b3e764c62b557189f3a1
Manisha-athili <manishaathili1126@gmail.com>
2025-02-18 01:13:54 +0530

    day 3

---
+++ day18/date.js
@@ -0,0 +1,47 @@
+// DAta methods
+
+
+let present = new Date()
+console.log(present.setDate()); // it helps to set a requires date
+console.log(present.getMonth()); // so if date is set only these will come
+console.log(present.getFullYear());
+
+
+let now = new Date();
+console.log(now.getMonth()+1);
+console.log(now.getFullYear());
+console.log(now.getDate())
+console.log(now.getMonth())
+console.log(now.getFullYear());
+console.log(now.getDay())
+// date methods::
+// getFullYear
+// getMonth
+// getDate
+
+// toLocalDateString :: returns full date as string
+// toLocalTimeString :: returns full time as string
+
+// SET METHODs
+ console.log("SET METHODs");
+ let set = new Date();
+console.log(set.setFullYear(2020));
+console.log(set.setDate(24));
+console.log(set.setTime(5));
+console.log(set.setHours(3)); 
+
+
+// MATH methods::
+console.log(Math.abs(-6.3));
+console.log(Math.ceil(4.5));  // rounds up to nxt largest int
+console.log(Math.floor(4.5)) // round dowm
+console.log(Math.round());
+console.log(Math.trunc(-4.33)); //returns only integer part not floating 
+// console.log(Math.pow(Number,expontent))
+console.log(Math.sqrt())
+console.log(Math.random())
+
+
+
+
+
